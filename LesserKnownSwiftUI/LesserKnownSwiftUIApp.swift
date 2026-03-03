d791239ec462bf02a67d785ebc64d942c66fbd18
superkookai <weerawut@live.com>
2026-03-03 10:46:49 +0700

    Initial Commit

---
+++ LesserKnownSwiftUI/LesserKnownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LesserKnownSwiftUIApp.swift
+//  LesserKnownSwiftUI
+//
+//  Created by Weerawut on 3/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct LesserKnownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
